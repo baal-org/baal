0d17594e08c8cc46d85c6290c7a330125c03a164
Jan Freyberg <jan.freyberg@gmail.com>
2019-10-28 17:17:42 +0000

    check that returned types match on to_cuda

--- tests/utils/test_cuda_utils.py
+++ tests/utils/test_cuda_utils.py
@@ -1,7 +1,7 @@
 import os
 import pytest
 import torch
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
 
 from baal.utils.cuda_utils import to_cuda
 
@@ -14,14 +14,23 @@ def test_to_cuda():
     assert next(to_cuda(m).parameters()).device.type == "cuda"
 
     t_list = [t, t.clone()]
+    assert type(t_list) is type(to_cuda(t_list))
     assert all(t_.device.type == "cuda" for t_ in to_cuda(t_list))
 
     t_dict = {"t": t, "t2": t.clone()}
+    assert type(t_dict) is type(to_cuda(t_dict))
     assert all(t_.device.type == "cuda" for t_ in to_cuda(t_dict).values())
 
     MyTuple = namedtuple("MyTuple", ["t", "t2"])
     t_named_tuple = MyTuple(t, t.clone())
+    assert type(t_named_tuple) is type(to_cuda(t_named_tuple))
     assert all(t_.device.type == "cuda" for t_ in to_cuda(t_named_tuple))
 
     t_tuple = (t, t.clone())
+    assert type(t_tuple) is type(to_cuda(t_tuple))
     assert all(t_.device.type == "cuda" for t_ in to_cuda(t_tuple))
+
+    # test a type that's not explicitly defined:
+    t_ordered_dict = OrderedDict([("t", t), ("t2", t.clone())])
+    assert type(t_ordered_dict) is type(to_cuda(t_ordered_dict))
+    assert all(t_.device.type == "cuda" for t_ in to_cuda(t_ordered_dict).values())
