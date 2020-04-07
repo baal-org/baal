6dd200134d6588bfa28fa7369fdfc89fa01c1cf2
Fred Branchaud <frederic.branchaud-charron@elementai.com>
2020-04-07 16:19:24 -0400

    Fix BatchBALD, now same results as official impl

--- src/baal/active/heuristics/heuristics.py
+++ src/baal/active/heuristics/heuristics.py
@@ -1,11 +1,12 @@
 import types
 import warnings
+from collections.abc import Sequence
 from functools import wraps as _wraps
 from typing import List
-from collections.abc import Sequence
 
 import numpy as np
 import scipy.stats
+import torch
 from scipy.special import xlogy
 from torch import Tensor
 
@@ -66,6 +67,31 @@ def requireprobs(fn):
         return fn(self, probabilities)
 
     return wrapper
+
+
+def gather_expand(data, dim, index):
+    """
+    Gather indices `index` from `data` after expanding along dimension `dim`.
+
+    Args:
+        data (tensor): A tensor of data.
+        dim (int): dimension to expand along.
+        index (tensor): tensor with the indices to gather.
+
+    Returns:
+        Tensor with the same shape as `index`.
+    """
+    max_shape = [max(dr, ir) for dr, ir in zip(data.shape, index.shape)]
+    new_data_shape = list(max_shape)
+    new_data_shape[dim] = data.shape[dim]
+
+    new_index_shape = list(max_shape)
+    new_index_shape[dim] = index.shape[dim]
+
+    data = data.expand(new_data_shape)
+    index = index.expand(new_index_shape)
+
+    return torch.gather(data, dim, index)
 
 
 class AbstractHeuristic:
@@ -228,6 +254,8 @@ class BatchBALD(BALD):
 
     Args:
         num_samples (int): Number of samples to select. (min 2*the amount of samples you want)
+        num_draw (int): Number of draw to perform from the history.
+                        From the paper `40000 // num_classes` is suggested.
         shuffle_prop (float): Amount of noise to put in the ranking. Helps with selection bias
             (default: 0.0).
         reduction (Union[str, callable]): function that aggregates the results
@@ -241,11 +269,49 @@ class BatchBALD(BALD):
         Not tested on 4+ dims.
         """
 
-    def __init__(self, num_samples, shuffle_prop=0.0, reduction='none'):
+    def __init__(self, num_samples, num_draw=500, shuffle_prop=0.0, reduction='none'):
         self.epsilon = 1e-5
         self.num_samples = num_samples
+        self.num_draw = num_draw
         super().__init__(shuffle_prop=shuffle_prop,
                          reduction=reduction)
+
+    def _draw_choices(self, probs, n_choices):
+        """
+        Draw `n_choices` sample from `probs`.
+
+        Returns:
+            choices: B... x `n_choices`
+        """
+        probs = probs.permute(0, 2, 1)
+        probs_B_C = probs.reshape((-1, probs.shape[-1]))
+
+        # samples: Ni... x draw_per_xx
+        choices = torch.multinomial(probs_B_C,
+                                    num_samples=n_choices, replacement=True)
+
+        choices_b_M = choices.reshape(list(probs.shape[:-1]) + [n_choices])
+        return choices_b_M.long()
+
+    def _sample_from_history(self, probs, num_draw=1000):
+        probs = torch.from_numpy(probs).double()
+
+        n_iterations = probs.shape[-1]
+
+        # [batch, draw, iterations]
+        choices = self._draw_choices(probs, num_draw)
+
+        # [batch, iterations, iterations, draw]
+        expanded_choices_N_K_K_S = choices[:, None, :, :]
+        expanded_probs_N_K_K_C = probs.permute(0, 2, 1)[:, :, None, :]
+
+        probs = gather_expand(expanded_probs_N_K_K_C, dim=-1, index=expanded_choices_N_K_K_S)
+        # exp sum log seems necessary to avoid 0s?
+        entropies = torch.exp(torch.sum(torch.log(probs), dim=0, keepdim=False))
+        entropies = entropies.reshape((n_iterations, -1))
+
+        samples_M_K = entropies.t()
+        return samples_M_K.numpy()
 
     def _conditional_entropy(self, probs):
         K = probs.shape[-1]
@@ -253,15 +319,20 @@ class BatchBALD(BALD):
 
     def _joint_entropy(self, predictions, selected):
         K = predictions.shape[-1]
+        C = predictions.shape[1]
+        B = predictions.shape[0]
         M = selected.shape[0]
+        predictions = predictions.swapaxes(1, 2)
 
         exp_y = np.array(
-            [np.matmul(selected, predictions[i].T) for i in range(predictions.shape[0])]) / K
+            [np.matmul(selected, predictions[i]) for i in range(predictions.shape[0])]) / K
+        assert exp_y.shape == (B, M, C)
         mean_entropy = selected.mean(-1, keepdims=True)[None]
+        assert mean_entropy.shape == (1, M, 1)
 
         step = 256
         for idx in range(0, exp_y.shape[0], step):
-            b_preds = exp_y[idx: (idx + step)]
+            b_preds = exp_y[(idx * step): ((idx + 1) * step)]
             yield np.sum(-xlogy(b_preds, b_preds) / mean_entropy, axis=(1, -1)) / M
 
     @requireprobs
@@ -275,29 +346,31 @@ class BatchBALD(BALD):
         Returns:
             Array of scores.
         """
-        MAX_SELECTED = 16000
         MIN_SPREAD = 0.1
         COUNT = 0
         # Get conditional_entropies_B
         conditional_entropies_B = self._conditional_entropy(predictions)
         bald_out = super().compute_score(predictions)
-        history = [self.reduction(bald_out).argmax()]
+        # We start with the most uncertain sample according to BALD.
+        history = self.reduction(bald_out).argsort()[-1:].tolist()
         for step in range(2 * self.num_samples):
-            # Select M, iterations example from history, take entropy
-            selected = predictions[np.random.permutation(history)[:MAX_SELECTED]]
-            selected = self.reduction(scipy.stats.entropy(np.swapaxes(selected, 0, 1)))
+            # Draw `num_draw` example from history, take entropy
+            # TODO use numpy/numba
+            selected = self._sample_from_history(predictions[history], num_draw=self.num_draw)
+
             # Compute join entropy
             joint_entropy = list(self._joint_entropy(predictions, selected))
             joint_entropy = np.concatenate(joint_entropy)
 
             partial_multi_bald_b = joint_entropy - conditional_entropies_B
             partial_multi_bald_b = self.reduction(partial_multi_bald_b)
-            partial_multi_bald_b[..., np.array(history)] = -1
+            partial_multi_bald_b[..., np.array(history)] = -1000
             # Add best to history
             partial_multi_bald_b = partial_multi_bald_b.squeeze()
             assert partial_multi_bald_b.ndim == 1
             winner_index = partial_multi_bald_b.argmax()
             history.append(winner_index)
+
             if partial_multi_bald_b.max() < MIN_SPREAD:
                 COUNT += 1
                 if COUNT > 50 or len(history) >= predictions.shape[0]:
@@ -499,6 +572,7 @@ class CombineHeuristics(AbstractHeuristic):
        shuffle_prop (float): shuffle proportion.
 
     """
+
     def __init__(self, heuristics: List, weights: List, reduction='mean', shuffle_prop=0.0):
         super(CombineHeuristics, self).__init__(reduction=reduction, shuffle_prop=shuffle_prop)
         self.composed_heuristic = heuristics
