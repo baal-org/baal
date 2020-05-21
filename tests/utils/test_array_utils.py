e6fc012681dcf32a56e5bfb5c4db38fa04332320
Fred Branchaud <frederic.branchaud-charron@elementai.com>
2020-05-21 14:01:28 -0400

    style

--- tests/utils/test_array_utils.py
+++ tests/utils/test_array_utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 import torch
 from scipy.special import softmax, expit
@@ -5,15 +6,17 @@ from scipy.special import softmax, expit
 from baal.utils import array_utils
 from baal.utils.array_utils import to_prob
 from baal.utils.iterutils import map_on_tensor
-import numpy as np
+
 
 @pytest.fixture()
 def a_tensor():
     return torch.randn([10, 3, 32, 32])
 
+
 @pytest.fixture()
 def an_array():
     return np.random.randn(10, 3, 32, 32)
+
 
 @pytest.fixture()
 def a_binary_array():
@@ -33,6 +36,7 @@ def test_stack_in_memory_multi(a_tensor):
     assert out[0].shape == (10 * iterations, 3, 32, 32)
     assert out[1].shape == (10 * iterations, 3, 32, 32)
 
+
 def test_to_prob(an_array, a_binary_array):
     out = to_prob(an_array)
     assert not np.allclose(out, an_array)
@@ -48,9 +52,6 @@ def test_to_prob(an_array, a_binary_array):
 
     out = to_prob(a_binary_array_scaled)
     assert np.allclose(out, a_binary_array_scaled)
-
-
-
 
 
 if __name__ == '__main__':
