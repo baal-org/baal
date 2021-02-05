e16bef49907f68c511c1a7631d0ad073e146a704
Parmida Atighehchian <parmida@elementai.com>
2021-02-05 16:58:37 -0500

    change name

--- src/baal/__init__.py
+++ src/baal/__init__.py
@@ -1,6 +1,6 @@
 from .version import __version__
 from .modelwrapper import ModelWrapper
-from .transformerswrapper import BaalHuggingFaceTrainer
+from .huggingface_trainer_wrapper import BaalHuggingFaceTrainer
 from .utils.log_configuration import set_logger_config
 
 set_logger_config()
