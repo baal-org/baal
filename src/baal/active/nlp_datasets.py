47fa16441d893fadc0edbc33028d7927d09dcd68
parmida.atighehchian <parmida.atighehchian@servicenow.com>
2021-08-11 12:34:41 -0400

    more clear

--- src/baal/active/nlp_datasets.py
+++ src/baal/active/nlp_datasets.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from torch.utils.data import Dataset
+from datasets import Dataset as HFDataset
 
 
 class HuggingFaceDatasets(Dataset):
@@ -19,7 +20,7 @@ class HuggingFaceDatasets(Dataset):
     """
 
     def __init__(self,
-                 dataset,
+                 dataset: HFDataset,
                  tokenizer=None,
                  target_key: str = "label",
                  input_key: str = "sentence",
@@ -60,3 +61,4 @@ class HuggingFaceDatasets(Dataset):
                 'attention_mask':
                     self.attention_masks[idx].flatten() if len(self.attention_masks) > 0 else None,
                 'label': torch.tensor(target, dtype=torch.long)}
+
