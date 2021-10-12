a28c290884796085c9912f21c794f4c4e6a1ac0a
fr.branchaud-charron <fr.branchaud-charron@servicenow.com>
2021-10-12 10:49:09 -0400

    Fix loop

--- experiments/lightning_flash_example.py
+++ experiments/lightning_flash_example.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch.backends
 from torch.nn import CrossEntropyLoss
 from torchvision import datasets
@@ -6,13 +5,13 @@ from torchvision.transforms import transforms
 
 try:
     import flash
-except ImportError:
+except ImportError as e:
+    print(e)
     raise ImportError(
         "`lightning-flash` library is required to run this example."
         " pip install 'git+https://github.com/PyTorchLightning/lightning-flash.git#egg=lightning-flash[image]'"
     )
-from flash import DataModule
-from flash.image import ImageClassifier
+from flash.image import ImageClassifier, ImageClassificationData
 from flash.core.classification import Probabilities
 from flash.image.classification.integrations.baal import ActiveLearningDataModule, ActiveLearningLoop
 
@@ -31,21 +30,25 @@ test_transforms = transforms.Compose([transforms.Resize((IMG_SIZE, IMG_SIZE)),
                                                            (0.2023, 0.1994, 0.2010))])
 
 
-class DataModule_(DataModule):
+class DataModule_(ImageClassificationData):
 
     @property
     def num_classes(self):
         return 10
 
 
-train_set = datasets.CIFAR10(".", train=True, transform=train_transforms, download=True)
-test_set = datasets.CIFAR10(".", train=False, transform=test_transforms, download=True)
-dm = DataModule_(train_dataset=train_set, test_dataset=test_set)
+train_set = datasets.CIFAR10(".", train=True, download=True)
+test_set = datasets.CIFAR10(".", train=False, download=True)
+dm = DataModule_.from_datasets(train_dataset=train_set,
+                               test_dataset=test_set,
+                               train_transform=train_transforms,
+                               val_transform=test_transforms,
+                               test_transform=test_transforms)
 active_dm = ActiveLearningDataModule(dm,
                                      heuristic=get_heuristic('bald'),
-                                     num_label_randomly=1024,
+                                     initial_num_labels=1024,
+                                     query_size=100,
                                      val_split=0.1)
-
 
 loss_fn = CrossEntropyLoss()
 head = torch.nn.Sequential(
@@ -62,7 +65,7 @@ model = ImageClassifier(num_classes=10,
                                           "momentum": 0.9,
                                           "weight_decay": 0},
                         learning_rate=1,  # we don't use learning rate here since it is initialized in the optimizer.
-                        serializer=Probabilities(),)
+                        serializer=Probabilities(), )
 
 if __name__ == "__main__":
     trainer = flash.Trainer(max_epochs=2500,
