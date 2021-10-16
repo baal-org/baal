2aefcd01e29f108e642b7d499e059ad56c9b84f0
Dref360 <frederic.branchaud.charron@gmail.com>
2021-10-16 13:37:50 -0400

    Seems to work now

--- experiments/pytorch_lightning/lightning_flash_example.py
+++ experiments/pytorch_lightning/lightning_flash_example.py
@@ -1,10 +1,14 @@
+from typing import Any
+
 import torch
 import argparse
 import os
 
 import torch.backends
 from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.trainer.states import TrainerFn, TrainerStatus, RunningStage
 from torch import nn
+from torch.utils.data import Subset
 from torchvision import datasets
 from torchvision.transforms import transforms
 
@@ -49,7 +53,7 @@ def get_data_module(heuristic, data_path):
                                    test_dataset=test_set,
                                    train_transform=train_transforms,
                                    test_transform=test_transforms,
-                                   batch_size=64,)
+                                   batch_size=64, )
     active_dm = ActiveLearningDataModule(dm,
                                          heuristic=get_heuristic(heuristic),
                                          initial_num_labels=1024,
@@ -85,15 +89,59 @@ def get_model(dm):
     return model
 
 
+class MyActiveLearningLoop(ActiveLearningLoop):
+    def advance(self, *args: Any, **kwargs: Any) -> None:
+        self.progress.increment_started()
+
+        if self.trainer.datamodule.has_labelled_data:
+            self.fit_loop.run()
+
+        if self.trainer.datamodule.has_test:
+            self._reset_testing()
+            metrics = self.trainer.test_loop.run()[0]
+            self.trainer.logger.log_metrics(metrics, step=self.trainer.global_step)
+
+        if self.trainer.datamodule.has_unlabelled_data:
+            self._reset_predicting()
+            probabilities = self.trainer.predict_loop.run()
+            self.trainer.datamodule.label(probabilities=probabilities)
+        else:
+            raise StopIteration
+
+        self._reset_fitting()
+        self.progress.increment_processed()
+
+    def on_run_start(self, *args: Any, **kwargs: Any) -> None:
+        super().on_run_start(*args, **kwargs)
+        self.trainer.test_loop._return_predictions = True
+
+    def _reset_testing(self):
+        self.trainer.state.fn = TrainerFn.TESTING
+        self.trainer.state.status = TrainerStatus.RUNNING
+        self.trainer.testing = True
+        self.trainer.lightning_module.on_test_dataloader()
+        self.trainer.accelerator.connect(self._lightning_module)
+
+    def on_advance_start(self, *args: Any, **kwargs: Any) -> None:
+        if self.trainer.datamodule.has_labelled_data:
+            self._reset_dataloader_for_stage(RunningStage.TRAINING)
+            self._reset_dataloader_for_stage(RunningStage.VALIDATING)
+            self._reset_dataloader_for_stage(RunningStage.TESTING)
+        if self.trainer.datamodule.has_unlabelled_data:
+            self._reset_dataloader_for_stage(RunningStage.PREDICTING)
+        self.progress.increment_ready()
+
+
 def main(args):
     gpus = 1 if torch.cuda.is_available() else 0
     active_dm = get_data_module(args.heuristic, args.data_path)
     model = get_model(active_dm.labelled)
-    logger = TensorBoardLogger(os.path.join(args.ckpt_path, "tensorboard"), name=f"flash-example-cifar-{args.heuristic}")
+    logger = TensorBoardLogger(os.path.join(args.ckpt_path, "tensorboard"),
+                               name=f"flash-example-cifar-{args.heuristic}")
     trainer = flash.Trainer(gpus=gpus, max_epochs=2500,
-                            default_root_dir=args.ckpt_path, logger=logger, limit_val_batches=0,)
-    active_learning_loop = ActiveLearningLoop(label_epoch_frequency=40,
-                                              inference_iteration=20)
+                            default_root_dir=args.ckpt_path, logger=logger, limit_val_batches=0, )
+    active_learning_loop = MyActiveLearningLoop(label_epoch_frequency=40,
+                                                inference_iteration=20)
     active_learning_loop.connect(trainer.fit_loop)
     trainer.fit_loop = active_learning_loop
     trainer.finetune(model, datamodule=active_dm, strategy="freeze")
