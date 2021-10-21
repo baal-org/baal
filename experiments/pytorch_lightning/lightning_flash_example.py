9f9202756a49457ecc8a7c26bc1e12ce4c284b45
fr.branchaud-charron <fr.branchaud-charron@servicenow.com>
2021-10-21 17:51:24 -0400

    reset epoch

--- experiments/pytorch_lightning/lightning_flash_example.py
+++ experiments/pytorch_lightning/lightning_flash_example.py
@@ -2,10 +2,13 @@ import argparse
 import os
 from typing import Any, List
 
+import numpy as np
+import structlog
 import torch
 import torch.backends
-import numpy as np
+from pytorch_lightning import seed_everything
 from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.trainer.progress import Progress
 from pytorch_lightning.trainer.states import TrainerFn, TrainerStatus, RunningStage
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
 from torch import nn
@@ -21,10 +24,12 @@ except ImportError as e:
         " pip install 'git+https://github.com/PyTorchLightning/lightning-flash.git#egg=lightning-flash[image]'"
     )
 from flash.image import ImageClassifier, ImageClassificationData
-from flash.core.classification import Probabilities
+from flash.core.classification import Logits
 from flash.image.classification.integrations.baal import ActiveLearningDataModule, ActiveLearningLoop
 
 from baal.active import get_heuristic
+
+log = structlog.get_logger()
 
 IMG_SIZE = 128
 
@@ -40,6 +45,7 @@ test_transforms = transforms.Compose([transforms.Resize((IMG_SIZE, IMG_SIZE)),
 
 
 class MyActiveLearningDataModule(ActiveLearningDataModule):
+
     def label(self, probabilities: List[torch.Tensor] = None, indices=None):
         if probabilities is not None and indices:
             raise MisconfigurationException(
@@ -70,8 +76,8 @@ def get_data_module(heuristic, data_path):
     active_dm = MyActiveLearningDataModule(dm,
                                            heuristic=get_heuristic(heuristic),
                                            initial_num_labels=1024,
-                                           query_size=250,
-                                           val_split=0.01)
+                                           query_size=200,
+                                           val_split=0.0)
     assert active_dm.has_test, "No test set?"
     return active_dm
 
@@ -79,54 +85,64 @@ def get_data_module(heuristic, data_path):
 def get_model(dm):
     loss_fn = nn.CrossEntropyLoss()
     head = nn.Sequential(
-        nn.Linear(512, 4096),
+        nn.Linear(512, 512),
         nn.ReLU(True),
         nn.Dropout(),
-        nn.Linear(4096, 4096),
+        nn.Linear(512, 512),
         nn.ReLU(True),
         nn.Dropout(),
-        nn.Linear(4096, dm.num_classes),
+        nn.Linear(512, dm.num_classes),
     )
+    LR = 0.001
     model = ImageClassifier(num_classes=dm.num_classes,
                             head=head,
                             backbone="vgg16",
                             pretrained=True,
                             loss_fn=loss_fn,
                             optimizer=torch.optim.SGD,
-                            optimizer_kwargs={"lr": 0.001,
+                            optimizer_kwargs={"lr": LR,
                                               "momentum": 0.9,
                                               "weight_decay": 5e-4},
-                            learning_rate=0.001,
-                            # we don't use learning rate here since it is initialized in the optimizer.
-                            serializer=Probabilities(), )
+                            learning_rate=LR,
+                            serializer=Logits(), )
     return model
 
 
 class MyActiveLearningLoop(ActiveLearningLoop):
     def advance(self, *args: Any, **kwargs: Any) -> None:
         self.progress.increment_started()
+        if len(self.trainer.datamodule._dataset) > 15000:
+            raise StopIteration
 
         if self.trainer.datamodule.has_labelled_data:
+            log.info("Training!")
             self.fit_loop.run()
 
         if self.trainer.datamodule.has_test:
             self._reset_testing()
+            log.info("Testing!")
             metrics = self.trainer.test_loop.run()[0]
             self.trainer.logger.log_metrics(metrics, step=self.trainer.global_step)
 
         if self.trainer.datamodule.has_unlabelled_data:
             self._reset_predicting()
+            log.info("Predicting!")
             probabilities = self.trainer.predict_loop.run()
             self.trainer.datamodule.label(probabilities=probabilities)
         else:
             raise StopIteration
 
         self._reset_fitting()
+
         self.progress.increment_processed()
 
     def on_run_start(self, *args: Any, **kwargs: Any) -> None:
         super().on_run_start(*args, **kwargs)
         self.trainer.test_loop._return_predictions = True
+
+    def _reset_fitting(self):
+        super()._reset_fitting()
+        self.fit_loop.epoch_progress = Progress()
 
     def _reset_testing(self):
         self.trainer.state.fn = TrainerFn.TESTING
@@ -138,7 +154,7 @@ class MyActiveLearningLoop(ActiveLearningLoop):
     def on_advance_start(self, *args: Any, **kwargs: Any) -> None:
         if self.trainer.datamodule.has_labelled_data:
             self._reset_dataloader_for_stage(RunningStage.TRAINING)
-            self._reset_dataloader_for_stage(RunningStage.VALIDATING)
+            #self._reset_dataloader_for_stage(RunningStage.VALIDATING)
             self._reset_dataloader_for_stage(RunningStage.TESTING)
         if self.trainer.datamodule.has_unlabelled_data:
             self._reset_dataloader_for_stage(RunningStage.PREDICTING)
@@ -146,14 +162,15 @@ class MyActiveLearningLoop(ActiveLearningLoop):
 
 
 def main(args):
+    seed_everything(args.seed)
     gpus = 1 if torch.cuda.is_available() else 0
     active_dm = get_data_module(args.heuristic, args.data_path)
     model = get_model(active_dm.labelled)
     logger = TensorBoardLogger(os.path.join(args.ckpt_path, "tensorboard"),
-                               name=f"flash-example-cifar-{args.heuristic}")
+                               name=f"flash-example-cifar-{args.heuristic}-{args.seed}")
     trainer = flash.Trainer(gpus=gpus, max_epochs=2500,
                             default_root_dir=args.ckpt_path, logger=logger, limit_val_batches=0, )
-    active_learning_loop = MyActiveLearningLoop(label_epoch_frequency=10,
+    active_learning_loop = MyActiveLearningLoop(label_epoch_frequency=20,
                                                 inference_iteration=20)
     active_learning_loop.connect(trainer.fit_loop)
     trainer.fit_loop = active_learning_loop
@@ -165,6 +182,7 @@ def parse_args():
     parser.add_argument("--heuristic", default="bald", type=str)
     parser.add_argument("--data_path", default="/data", type=str)
     parser.add_argument("--ckpt_path", default="/ckpt", type=str)
+    parser.add_argument("--seed", default=2021, type=int)
     return parser.parse_args()
 
 
