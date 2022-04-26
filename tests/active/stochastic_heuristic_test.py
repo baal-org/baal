58ab69729111fcd48ff7f44568997c47a72d938f
fr.branchaud-charron <fr.branchaud-charron@servicenow.com>
2022-04-26 14:19:24 -0400

    Fix tests

--- tests/active/stochastic_heuristic_test.py
+++ tests/active/stochastic_heuristic_test.py
@@ -4,12 +4,19 @@ from scipy.stats import entropy
 
 from baal.active.heuristics import BALD, Entropy
 from baal.active.heuristics.stochastics import GibbsSampling, RankBasedSampling, PowerSampling
-from tests.test_utils import make_fake_dist
+
+NUM_CLASSES = 10
+NUM_ITERATIONS = 20
+BATCH_SIZE = 32
 
 
 @pytest.fixture
 def sampled_predictions():
-    return np.stack([make_fake_dist([1, 2, 2], [1, 3, 3], dims=20) for _ in range(10)])
+    predictions = np.stack(
+        [np.histogram(np.random.rand(5), bins=np.linspace(-.5, .5, NUM_CLASSES + 1))[0] for _ in
+         range(BATCH_SIZE * NUM_ITERATIONS)]).reshape(
+        [BATCH_SIZE, NUM_ITERATIONS, NUM_CLASSES])
+    return np.rollaxis(predictions, -1, 1)
 
 
 @pytest.mark.parametrize("stochastic_heuristic", [GibbsSampling, RankBasedSampling, PowerSampling])
@@ -17,9 +24,14 @@ def sampled_predictions():
 def test_stochastic_heuristic(stochastic_heuristic, base_heuristic, sampled_predictions):
     heur_temp_1 = stochastic_heuristic(base_heuristic(), query_size=100, temperature=1.0)
     heur_temp_10 = stochastic_heuristic(base_heuristic(), query_size=100, temperature=10.0)
+    heur_temp_05 = stochastic_heuristic(base_heuristic(), query_size=100, temperature=0.01)
 
     scores = heur_temp_1.get_scores(sampled_predictions)
 
-    dist_temp_1, dist_temp_10 = heur_temp_1._make_distribution(scores), heur_temp_10._make_distribution(scores)
+    dist_temp_1, dist_temp_10, dist_temp_05 = (heur_temp_1._make_distribution(scores),
+                                               heur_temp_10._make_distribution(scores),
+                                               heur_temp_05._make_distribution(scores))
 
-    assert entropy(dist_temp_1) > entropy(dist_temp_10)
+    assert entropy(dist_temp_1) < entropy(dist_temp_10)
+    # NOTE: it is possible that this fails, as temp_1 can already have minimal entropy. This is unlikely.
+    assert entropy(dist_temp_1) > entropy(dist_temp_05)
