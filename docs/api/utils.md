3a506fb5f451beccd437223d549c018b41c28181
Dref360 <frederic.branchaud.charron@gmail.com>
2022-07-31 11:00:23 -0400

    Add example in doc

--- docs/api/utils.md
+++ docs/api/utils.md
@@ -5,6 +5,32 @@
 
 To work with `baal.modelwrapper.ModelWrapper`, we provide `Metrics`.
 
+
+### Examples
+
+```python
+from baal.modelwrapper import ModelWrapper
+from baal.utils.metrics import Accuracy
+
+wrapper : ModelWrapper = ...
+# You can add any metrics from `baal.utils.metrics`.
+wrapper.add_metric(name='accuracy',initializer=lambda : Accuracy())
+
+# Metrics are automatically set when training and evaluating.
+wrapper.train_on_dataset(...)
+wrapper.test_on_dataset(...)
+
+print(wrapper.get_metrics())
+"""
+>>> {'dataset_size': 200,
+    'test_accuracy': 0.26038339734077454,
+    'test_loss': 2.190103769302368,
+    'train_accuracy': 0.3214285671710968,
+    'train_loss': 2.1795670986175537}
+...
+"""
+```
+
 ```eval_rst
 .. automodule:: baal.utils.metrics
     :members:
---
+++ tests/metrics/test_mixin.py
@@ -0,0 +1,52 @@
+import numpy as np
+import torch
+from baal.modelwrapper import ModelWrapper
+from baal.utils.metrics import Accuracy, Precision
+
+
+def test_active_step():
+    wrapper = ModelWrapper(None, None)
+    precisions = np.linspace(0,1, 10, endpoint=False)
+    recalls = np.linspace(0.5, 1, 10, endpoint=False)
+    dataset_size = list(range(100, 1100, 100))
+    for ds_size, precision, recall in zip(dataset_size, precisions, recalls):
+        wrapper.active_step(ds_size, {
+            'Precision': precision,
+            'Recall': recall
+        })
+    assert len(wrapper.active_learning_metrics) == 10
+    assert wrapper.active_learning_metrics[100] == {
+        'Precision': 0.0,
+        'Recall': 0.5
+    }
+    
+    wrapper = ModelWrapper(None, None)
+    wrapper.set_dataset_size(1000)
+    wrapper.active_step(dataset_size=None, metrics={
+        'Precision': 0.1,
+        'Recall': 0.2
+    })
+    assert wrapper._active_dataset_size == 1000
+    assert wrapper.active_learning_metrics == {
+        1000: {
+        'Precision': 0.1,
+        'Recall': 0.2
+    }
+    }
+
+def test_get_metrics():
+    wrapper = ModelWrapper(None, None)
+    wrapper.add_metric('accuracy', Accuracy)
+
+    assert len(wrapper.get_metrics()) == 4
+    assert len(wrapper.get_metrics('test')) == 2
+    assert all('test' in ki for ki in wrapper.get_metrics('test'))
+    assert len(wrapper.get_metrics('train')) == 2
+    assert all('train' in ki for ki in wrapper.get_metrics('train'))
+
+    wrapper.set_dataset_size(1000)
+    assert len(wrapper.get_metrics()) == 5
+    assert len(wrapper.get_metrics('test')) == 3
+    assert sum('test' in ki for ki in wrapper.get_metrics('test')) == 2
+    assert len(wrapper.get_metrics('train')) == 3
+    assert sum('train' in ki for ki in wrapper.get_metrics('train')) == 2
