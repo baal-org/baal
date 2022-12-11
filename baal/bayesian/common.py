b60879ea59d132d66a0f9d6c4ca5df576cdca5eb
Dref360 <frederic.branchaud.charron@gmail.com>
2022-12-11 12:58:22 -0500

    Fix type

--- baal/bayesian/common.py
+++ baal/bayesian/common.py
@@ -15,7 +15,7 @@ def replace_layers_in_module(module: nn.Module, mapping_fn: Callable, *args, **k
     """
     changed = False
     for name, child in module.named_children():
-        new_module: nn.Module = mapping_fn(child, *args, **kwargs)
+        new_module: Optional[nn.Module] = mapping_fn(child, *args, **kwargs)
 
         if new_module is not None:
             changed = True
