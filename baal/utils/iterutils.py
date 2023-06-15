48d73ef506af8e76c0979ba5ab27ea9cbd1bfe91
Dref360 <fred@glowstick.cx>
2023-06-15 14:12:01 -0400

    Handle NER usecase

--- baal/utils/iterutils.py
+++ baal/utils/iterutils.py
@@ -1,10 +1,10 @@
-from collections.abc import Sequence
+from collections.abc import Sequence, MutableMapping
 
 
 def map_on_tensor(fn, val):
     """Map a function on a Tensor or a list of Tensors"""
     if isinstance(val, Sequence):
-        return [fn(v) for v in val]
-    elif isinstance(val, dict):
-        return {k: fn(v) for k, v in val.items()}
+        return [map_on_tensor(fn, v) for v in val]
+    elif isinstance(val, (dict, MutableMapping)):
+        return {k: map_on_tensor(fn, v) for k, v in val.items()}
     return fn(val)
